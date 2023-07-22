f1526ed293c7988920fff87355786a893a9fe87f
Júlio Cezar Koronati <jckoronati@hotmail.com>
2023-07-22 19:52:31 -0300

    implements get books route

--- src/Routes/Books/booksRoutes.js
+++ src/Routes/Books/booksRoutes.js
@@ -5,6 +5,10 @@ const booksRouter = Router();
 
 const booksController = new BooksController();
 
-booksRouter.get('/', await booksController.getAll());
+booksRouter.get('/', async (req, res) => {
+    const result = await booksController.getAll();
+
+    res.status(200).json(result);
+});
 
 export { booksRouter };
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,6 +3,10 @@ import { booksRouter as books } from './Books/booksRoutes.js'
 
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.status(200).send('API successfully online');
+});
+
 router.use('/books', books);
 
 export { router };
