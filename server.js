79f01a3cb1589e1dfc18ee667a5bc084e9450408
Júlio Cezar Koronati <jckoronati@hotmail.com>
2023-07-22 11:58:59 -0300

    chore: add nodemon on project

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = 3000;
 
+const routes = {
+    '/': 'API',
+    '/books': 'Books ...',
+    '/authors': 'Authors list ...',
+};
+
 const server = http.createServer((req, res) => { 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('API is on');
+    res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
