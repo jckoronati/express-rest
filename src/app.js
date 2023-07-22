495532fa1f4089a607df7f60e06f159933b8b507
Júlio Cezar Koronati <jckoronati@hotmail.com>
2023-07-22 12:48:57 -0300

    create post route to books

--- src/app.js
+++ src/app.js
@@ -7,12 +7,20 @@ const books = [
     { id: 2, title: 'Coraline', author: 'Neil Gaiman' },
 ];
 
+app.use(express.json());
+
 app.get('/', (req, res) => { 
     res.status(200).send('API node');
 });
 
 app.get('/books', (req, res) => {
     res.status(200).json(books);
+});
+
+app.post('/books', (req, res) => { 
+    books.push(req.body);
+
+    res.status(201).send('The book was successfully registered');
 });
 
 export default app;
